37160979c24b5d9d210157b2711ac69ceebb3978
EdwinHrdz <edwin_arturo_hrdz@hotmail.com>
2024-06-07 21:56:07 -0700

    Se agrego la logica y el diseño de registro

---
+++ lib/views/login/register/components/register_textfield.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class RegisterTextField extends StatelessWidget {
+  final double width;
+  final IconData icon;
+  final String? Function(String?)? validator;
+  final void Function(String)? onChanged;
+  final String hintText;
+  final TextEditingController? controller;
+
+  const RegisterTextField({
+    Key? key,
+    required this.width,
+    required this.icon,
+    this.validator,
+    this.onChanged,
+    this.hintText = 'Ingrese el Texto',
+    this.controller,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 45,
+      width: width,
+      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      decoration: BoxDecoration(
+        color: Colors.transparent,
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+          color: Colors.black,
+          width: 2,
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: TextFormField(
+          controller: controller,
+          decoration: InputDecoration(
+            icon: Icon(icon, color: Colors.black, size: 30),
+            hintText: hintText,
+            border: InputBorder.none,
+          ),
+          validator: validator,
+          onChanged: onChanged,
+        ),
+      ),
+    );
+  }
+}
