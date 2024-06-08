23dc4f79f0e09cc168dd138debdddbc5ed1463a6
OmarCasas <omar.casasbarron@cesunbc.edu.mx>
2024-06-07 17:34:39 -0700

    Registro

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:store/views/register/register_desing.dart';
+// ignore: unused_import
 import 'views/login/login_design.dart';
 
 void main() {
@@ -17,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
         useMaterial3: true,
       ),
-      home: const LoginDesign(),
+      home: const Registro(),
     );
   }
 }
